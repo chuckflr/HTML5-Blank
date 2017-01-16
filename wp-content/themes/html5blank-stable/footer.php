0361ed82d21a1586fe68de355d133281446a8202
Carlos Flores <Carlos Flores>
2017-01-16 15:50:22 -0800

    First commit

---
+++ wp-content/themes/html5blank-stable/footer.php
@@ -0,0 +1,22 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+				<!-- Scripts -->
+			<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+			<script type="text/javascript" src="assets/js/skel.min.js"></script>
+			<script type="text/javascript" src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script type="text/javascript" src="assets/js/main.js"></script>
+
+	</body>
+</html>
